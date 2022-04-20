0a8a073a4c5e52a6d71acbe25acc8291905d5671
Tim Robles <robles.tim@gmail.com>
2022-04-20 10:48:22 -0500

    database

--- sistema_django_postgresql/settings/local.py
+++ sistema_django_postgresql/settings/local.py
@@ -8,5 +8,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR.child('db.sqlite3'),
+    },
+    'prod': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbprueba',
+        'USER': 'multiplay',
+        'PASSWORD': 'multiplay123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
