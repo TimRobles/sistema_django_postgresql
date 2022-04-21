add6da95c2c3bf476c1a758343ed9ec7239d603a
RonnyOre <ronny.oa.14@gmail.com>
2022-04-21 11:51:10 -0500

    mod_Agregue dato Condicion SUNAT

--- applications/sociedad/forms.py
+++ applications/sociedad/forms.py
@@ -14,6 +14,7 @@ class SociedadForm(BSModalModelForm):
             'direccion_legal',
             'ubigeo',
             'estado_sunat',
+            'condicion',
             )
 
     def __init__(self, *args, **kwargs):
