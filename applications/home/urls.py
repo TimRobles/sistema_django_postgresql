6843243af05bda7521479f676736d36598382dca
DianaSilvaD <silvaelizabethdiana@gmail.com>
2022-04-22 11:35:11 -0500

    22_Abril_Diana

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from applications.home.views import (
     ConsultaRucView,
-    HomePage
+    PanelView,
+    InicioView,
+    UserLogoutView,
 )
 
 app_name = 'home_app'
 
 urlpatterns = [
-    path('', HomePage.as_view(), name='home'),
+    path('', PanelView.as_view(), name='home'),
+    path('login/', InicioView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+
     path('consulta-ruc/<int:ruc>/', ConsultaRucView, name='consulta_ruc'),
 ]
--- applications/home/views.py
+++ applications/home/views.py
@@ -1,8 +1,59 @@
 from applications.funciones import consulta_dni, consulta_ruc
 from applications.importaciones import *
+from .forms import UserLoginForm
 
-class HomePage(TemplateView):
-    template_name = "home/home.html"
+
+class InicioView(FormView):
+    template_name = "home/inicio.html"
+    form_class = UserLoginForm
+
+    def form_valid(self, form):
+        user = authenticate(
+            username = form.cleaned_data['username'],
+            password = form.cleaned_data['password'],
+        )
+        login(self.request, user)
+        self.request.session['sociedad'] = form.cleaned_data['sociedad'].razon_social
+        '''self.request.session['logo'] = form.cleaned_data['sociedad'].logo.url
+        self.request.session['color'] = form.cleaned_data['sociedad'].color'''
+        self.request.session.set_expiry(0)
+        return super(InicioView, self).form_valid(form)
+
+    def get_success_url(self):
+        next = self.request.GET.get("next")
+        
+        if next:
+            return "%s" % (next)
+        else:
+            return reverse_lazy('home_app:home')
+
+class UserLogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return HttpResponseRedirect(reverse_lazy('home_app:login'))
+
+class PanelView(LoginRequiredMixin, TemplateView):
+    template_name = "home/panel.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(PanelView, self).get_context_data(**kwargs)
+        group_permissions = self.request.user.get_all_permissions()
+        context['Permisos'] = group_permissions
+        return context
+
+class SinPermisoView(TemplateView):
+    template_name = "403.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SinPermisoView, self).get_context_data(**kwargs)
+        context['META'] = self.request.META
+        if self.request.META.get('HTTP_REFERER') and self.request.META.get('HTTP_REFERER') != 'http://0.0.0.0:8000/sin-permiso/':
+            context['previous'] = self.request.META.get('HTTP_REFERER')
+        else:
+            context['previous'] = reverse_lazy('home_app:home')
+        return context
+    
+
 
 def ConsultaRucView(request, ruc):
     data = dict()
