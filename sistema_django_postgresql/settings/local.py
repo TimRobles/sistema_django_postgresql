42b26ec480bb48f24a1f44a2a4f161f3999adb6d
RonnyOre <ronny.oa.14@gmail.com>
2022-04-27 15:25:21 -0500

    Modificaciones

--- sistema_django_postgresql/settings/local.py
+++ sistema_django_postgresql/settings/local.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR.child('db.sqlite3'),
+        'NAME': BASE_DIR.ancestor(1).child('sistema_django_postgresql.sqlite3'),
     },
     'prod': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -26,4 +26,4 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.child('media')
+MEDIA_ROOT = BASE_DIR.ancestor(1).child('media_sistema_django_postgresql')
