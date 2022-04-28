556b3d9a1eca390a9d46f775b82b20f573edcbe2
RonnyOre <ronny.oa.14@gmail.com>
2022-04-28 13:02:58 -0500

    Recepción: Módulo de Visitas

--- applications/recepcion/urls.py
+++ applications/recepcion/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('visita/', views.VisitaListView.as_view(), name='visita_inicio'),
     path('visita-tabla/', views.VisitaTabla, name='visita_tabla'),
     path('visita/registrar/', views.VisitaCreateView.as_view(), name='visita_registrar'),
+    path('visita/registrar-salida/<pk>/', views.VisitaRegistrarSalidaView.as_view(), name='visita_registrar_salida'),
 ]
--- applications/recepcion/views.py
+++ applications/recepcion/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime, time
 from applications.importaciones import *
 
 from .forms import (
@@ -28,12 +29,18 @@ def VisitaTabla(request):
         )
         return JsonResponse(data)
 
-class VisitaCreateView(BSModalCreateView):
 
+class VisitaCreateView(BSModalCreateView):
     model = Visita
-    template_name = "includes/formulario generico.html"
+    template_name = "recepcion/visita/registrar.html"
     form_class = VisitaForm
     success_url = reverse_lazy('visita_app:visita_inicio')
+
+    def get_context_data(self, **kwargs):
+        context = super(VisitaCreateView, self).get_context_data(**kwargs)
+        context['accion']="Registrar"
+        context['titulo']="Visita"
+        return context
 
     def form_valid(self, form):
 
@@ -42,10 +49,24 @@ class VisitaCreateView(BSModalCreateView):
 
         return super().form_valid(form)
 
+class VisitaRegistrarSalidaView(BSModalDeleteView):
+    model = Visita
+    template_name = "includes/eliminar generico.html"
+    success_url = reverse_lazy('visita_app:visita_inicio')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        hour = datetime.now()
+        self.object.hora_salida = hour.strftime("%H:%M")
+        registro_guardar(self.object, self.request)
+        self.object.save()
+        messages.success(request, MENSAJE_REGISTRAR_SALIDA)
+        return HttpResponseRedirect(self.get_success_url())
+
     def get_context_data(self, **kwargs):
-        context = super(VisitaCreateView, self).get_context_data(**kwargs)
-        context['accion']="Registrar"
-        context['titulo']="Visita"
+        context = super(VisitaRegistrarSalidaView, self).get_context_data(**kwargs)
+        context['accion'] = "Registrar Salida"
+        context['titulo'] = "Visita"
+        context['dar_baja'] = "true"
+        context['item'] = self.object.nombre
         return context
-
-
