f3aaac85b805a537f12d841d5ec92b82b5ea02c4
Tim Robles <robles.tim@gmail.com>
2022-05-09 15:44:03 -0500

    Error en forms de programas

--- applications/programas/forms.py
+++ applications/programas/forms.py
@@ -14,6 +14,8 @@ class NivelUnoForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         max_orden = NivelUno.objects.aggregate(Max('orden'))['orden__max']
+        if max_orden == None:
+            max_orden = 0
         super(NivelUnoForm, self).__init__(*args, **kwargs)
         self.fields['orden'].initial = max_orden + 1
         
