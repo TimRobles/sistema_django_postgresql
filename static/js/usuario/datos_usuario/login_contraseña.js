79a991b6b7e84a0b82169b4eb2849b1a8ee90849
DianaSilvaD <silvaelizabethdiana@gmail.com>
2022-05-12 16:40:47 -0500

    inicio eye

---
+++ static/js/usuario/datos_usuario/login_contraseña.js
@@ -0,0 +1,25 @@
+function VerContraseña(evento) {
+    if (evento.tagName == "I") {
+        boton = evento.parentElement;
+    } else {
+        boton = evento;
+    }
+
+    if (boton.parentElement.children[1].type == "password") {
+        boton.parentElement.children[1].type = 'text';
+    } else {
+        boton.parentElement.children[1].type = "password";
+    }
+
+    if (boton.parentElement.children[2].children[0].className == "bi bi-eye-fill") {
+        boton.parentElement.children[2].children[0].className = 'bi bi-eye-slash-fill';
+    } else {
+        boton.parentElement.children[2].children[0].className = "bi bi-eye-fill";
+    }
+    
+    }
+
+$(document).on('click', '#ver-contraseña', function (e) {
+    VerContraseña(e.target);
+});
+
