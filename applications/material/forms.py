19b2d1f8810aba77ddd4b2be586a0b1ae39ea9ff
DianaSilvaD <silvaelizabethdiana@gmail.com>
2022-05-20 18:23:39 -0500

    materiales

--- applications/material/forms.py
+++ applications/material/forms.py
@@ -1,7 +1,8 @@
 from dataclasses import fields
 from django import forms
 
-from .models import Modelo, Marca
+from applications.datos_globales.models import SegmentoSunat,FamiliaSunat,ClaseSunat,ProductoSunat, Unidad
+from .models import Familia, Modelo, Marca, Material, RelacionMaterialComponente, Especificacion, Datasheet, SubFamilia
 
 from bootstrap_modal_forms.forms import BSModalForm, BSModalModelForm
 
@@ -34,3 +35,148 @@ class MarcaForm(BSModalModelForm):
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
         self.fields['modelos'].widget.attrs['class'] = 'nobull'
+
+class MaterialForm(BSModalModelForm):
+    familia = forms.ModelChoiceField(queryset=Familia.objects.all())
+    class Meta:
+        model = Material
+        fields=(
+            'descripcion_venta',
+            'descripcion_corta',
+            'familia',
+            'subfamilia',
+            'unidad_base',
+            'peso_unidad_base',
+            'marca',
+            'modelo',
+            'clase',
+            'control_serie',
+            'control_lote',
+            'control_calidad',
+            'mostrar',
+            )
+
+    def __init__(self, *args, **kwargs):
+        super(MaterialForm, self).__init__(*args, **kwargs)          
+        self.fields['subfamilia'].queryset = SubFamilia.objects.none()
+        self.fields['unidad_base'].queryset = Unidad.objects.none()
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+        self.fields['control_serie'].widget.attrs['class'] = 'form-check-input'
+        self.fields['control_lote'].widget.attrs['class'] = 'form-check-input'
+        self.fields['control_calidad'].widget.attrs['class'] = 'form-check-input'
+        self.fields['mostrar'].widget.attrs['class'] = 'form-check-input'
+
+class RelacionMaterialComponenteForm(BSModalModelForm):
+    class Meta:
+        model = RelacionMaterialComponente
+        fields=(
+            'componentematerial',
+            'cantidad',
+            )
+
+    def __init__(self, *args, **kwargs):
+        componentes = kwargs.pop('componentes')
+        super(RelacionMaterialComponenteForm, self).__init__(*args, **kwargs)
+        self.fields['componentematerial'].queryset = componentes
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+   
+class EspecificacionForm(BSModalModelForm):
+    class Meta:
+        model = Especificacion
+        fields=(
+            'orden',
+            'atributomaterial',
+            'valor',
+            )
+
+    def __init__(self, *args, **kwargs):
+        atributos = kwargs.pop('atributos')
+        super(EspecificacionForm, self).__init__(*args, **kwargs)   
+        self.fields['atributomaterial'].queryset = atributos
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+class DatasheetForm(BSModalModelForm):
+    class Meta:
+        model = Datasheet
+        fields=(
+            'descripcion',
+            'archivo',
+            )
+
+    def __init__(self, *args, **kwargs):
+        super(DatasheetForm, self).__init__(*args, **kwargs)          
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+class DatosImportacionForm(BSModalModelForm):
+    class Meta:
+        model = Material
+        fields=(
+            'traduccion',
+            'partida',
+            'uso_funcion',
+            'compuesto_por',
+            'es_componente',
+            )
+
+    def __init__(self, *args, **kwargs):
+        super(DatosImportacionForm, self).__init__(*args, **kwargs)          
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+        self.fields['es_componente'].widget.attrs['class'] = 'form-check-input'
+
+class ProductoSunatForm(BSModalModelForm):
+    segmento_sunat = forms.ModelChoiceField(label = 'Segmento Sunat', queryset = SegmentoSunat.objects.all(), required=False)
+    familia_sunat = forms.ModelChoiceField(label = 'Familia Sunat', queryset = FamiliaSunat.objects.none(), required=False)
+    clase_sunat = forms.ModelChoiceField(label = 'Clase Sunat', queryset = ClaseSunat.objects.none(), required=False)
+    producto_sunat = forms.ModelChoiceField(label = 'Producto Sunat', queryset = ProductoSunat.objects.none(), required=False)
+    class Meta:
+        model = Material
+        fields=(
+            'segmento_sunat',
+            'familia_sunat',
+            'clase_sunat',
+            'producto_sunat',
+            )
+
+    def clean_segmento_sunat(self):
+        segmento_sunat = self.cleaned_data.get('segmento_sunat')
+        familia_sunat = self.fields['familia_sunat']
+        familia_sunat.queryset = FamiliaSunat.objects.filter(segmento = segmento_sunat.codigo)
+        
+        return segmento_sunat
+
+    def clean_familia_sunat(self):
+        familia_sunat = self.cleaned_data.get('familia_sunat')
+        clase_sunat = self.fields['clase_sunat']
+        clase_sunat.queryset = ClaseSunat.objects.filter(familia = familia_sunat.codigo)
+    
+        return familia_sunat
+
+    def clean_clase_sunat(self):
+        clase_sunat = self.cleaned_data.get('clase_sunat')
+        producto_sunat = self.fields['producto_sunat']
+        producto_sunat.queryset = ProductoSunat.objects.filter(clase = clase_sunat.codigo)
+    
+        return clase_sunat
+
+    def __init__(self, *args, **kwargs):
+        super(ProductoSunatForm, self).__init__(*args, **kwargs)
+        producto_sunat = self.instance.producto_sunat
+        if producto_sunat:
+            clase_sunat = producto_sunat.clase
+            familia_sunat = clase_sunat.familia
+            segmento_sunat = familia_sunat.segmento
+            self.fields['segmento_sunat'].initial = segmento_sunat
+            self.fields['familia_sunat'].queryset = FamiliaSunat.objects.filter(segmento = segmento_sunat.codigo)
+            self.fields['familia_sunat'].initial = familia_sunat
+            self.fields['clase_sunat'].queryset = ClaseSunat.objects.filter(familia = familia_sunat.codigo)
+            self.fields['clase_sunat'].initial = clase_sunat
+            self.fields['producto_sunat'].queryset = ProductoSunat.objects.filter(clase = clase_sunat.codigo)
+            self.fields['producto_sunat'].initial = producto_sunat
+        
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
--- applications/material/views.py
+++ applications/material/views.py
@@ -1,10 +1,19 @@
 from applications.importaciones import *
+from applications.datos_globales.models import SegmentoSunat,FamiliaSunat,ClaseSunat,ProductoSunat
+from django import forms
+from django.shortcuts import render
+
 from .forms import (
-    ModeloForm, MarcaForm
+    ModeloForm, MarcaForm,MaterialForm, 
+    RelacionMaterialComponenteForm,EspecificacionForm, DatasheetForm,DatosImportacionForm,ProductoSunatForm,
     )
 from .models import (
     Modelo, 
     Marca,
+    Material,
+    RelacionMaterialComponente,
+    Especificacion,
+    Datasheet,
     )
 
 class ModeloListView(PermissionRequiredMixin, ListView):
@@ -130,3 +139,362 @@ class MarcaUpdateView(PermissionRequiredMixin, BSModalUpdateView):
         registro_guardar(form.instance, self.request)
         
         return super().form_valid(form)
+
+
+class MaterialListView(ListView):
+    model = Material
+    template_name = "material/material/inicio.html"
+    context_object_name = 'contexto_material'
+
+def MaterialTabla(request):
+    data = dict()
+    if request.method == 'GET':
+        template = 'material/material/inicio_tabla.html'
+        context = {}
+        context['contexto_material'] = Material.objects.all()
+
+        data['table'] = render_to_string(
+            template,
+            context,
+            request=request
+        )
+        return JsonResponse(data)
+
+class MaterialCreateView(BSModalCreateView):
+    model = Material
+    template_name = "includes/formulario generico.html"
+    form_class = MaterialForm 
+    success_url = reverse_lazy('material_app:material_inicio')
+
+    def get_context_data(self, **kwargs):
+        context = super(MaterialCreateView, self).get_context_data(**kwargs)
+        context['accion']="Registrar"
+        context['titulo']="Material"
+        return context
+        
+
+    def form_valid(self, form):
+
+        form.instance.usuario = self.request.user
+        registro_guardar(form.instance, self.request)
+
+        return super().form_valid(form)
+
+class MaterialUpdateView(BSModalUpdateView):
+    model = Material
+    template_name = "includes/formulario generico.html"
+    form_class = MaterialForm
+    success_url = reverse_lazy('material_app:material_inicio')
+
+    def get_context_data(self, **kwargs):
+        context = super(MaterialUpdateView, self).get_context_data(**kwargs)
+        context['accion'] = "Actualizar"
+        context['titulo'] = "Material"
+        return context
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        registro_guardar(form.instance, self.request)
+        
+        return super().form_valid(form)
+
+class MaterialDarBajaView(BSModalDeleteView):
+    model = Material
+    template_name = "includes/eliminar generico.html"
+    success_url = reverse_lazy('material_app:material_inicio')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.estado_alta_baja = 2
+        registro_guardar(self.object, self.request)
+        self.object.save()
+        messages.success(request, MENSAJE_DAR_BAJA)
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_context_data(self, **kwargs):
+        context = super(MaterialDarBajaView, self).get_context_data(**kwargs)
+        context['accion'] = "Dar Baja"
+        context['titulo'] = "Materiales"
+        context['dar_baja'] = "true"
+        context['item'] = self.object.descripcion_venta
+        return context
+
+class MaterialDarAltaView(BSModalDeleteView):
+    model = Material
+    template_name = "includes/eliminar generico.html"
+    success_url = reverse_lazy('material_app:material_inicio')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.estado_alta_baja = 1
+        registro_guardar(self.object, self.request)
+        self.object.save()
+        messages.success(request, MENSAJE_DAR_ALTA)
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_context_data(self, **kwargs):
+        context = super(MaterialDarAltaView, self).get_context_data(**kwargs)
+        context['accion']="Dar Alta"
+        context['titulo'] = "Material"
+        context['dar_baja'] = "true"
+        context['item'] = self.object.descripcion_venta
+        return context
+
+
+class MaterialDetailView(DetailView):
+    model = Material
+    template_name = "material/material/detalle.html"
+    context_object_name = 'contexto_material'
+
+    def get_context_data(self, **kwargs):
+        material = Material.objects.get(id = self.kwargs['pk'])
+        context = super(MaterialDetailView, self).get_context_data(**kwargs)
+        context['componentes'] = RelacionMaterialComponente.objects.filter(material = material)
+        context['especificaciones'] = Especificacion.objects.filter(material = material)
+        context['datasheets'] = Datasheet.objects.filter(material = material)
+        return context
+
+def MaterialDetailTabla(request, pk):
+    data = dict()
+    if request.method == 'GET':
+        template = 'material/material/detalle_tabla.html'
+        context = {}
+        material = Material.objects.get(id = pk)
+        context['contexto_material'] = material
+        context['componentes'] = RelacionMaterialComponente.objects.filter(material = material)
+        context['especificaciones'] = Especificacion.objects.filter(material = material)
+        context['datasheets'] = Datasheet.objects.filter(material = material)
+
+        data['table'] = render_to_string(
+            template,
+            context,
+            request=request
+        )
+        return JsonResponse(data)
+
+class ComponenteCreateView(BSModalCreateView):
+    model = RelacionMaterialComponente
+    template_name = "includes/formulario generico.html"
+    form_class = RelacionMaterialComponenteForm
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('material_app:material_detalle', kwargs={'pk':self.kwargs['material_id']})
+
+    def form_valid(self, form):
+        form.instance.material = Material.objects.get(id = self.kwargs['material_id'])
+        form.instance.usuario = self.request.user
+        registro_guardar(form.instance, self.request)
+        return super().form_valid(form)
+
+    def get_form_kwargs(self, *args, **kwargs):
+        material = Material.objects.get(id = self.kwargs['material_id'])
+        kwargs = super(ComponenteCreateView, self).get_form_kwargs(*args, **kwargs)
+        kwargs['componentes'] = material.subfamilia.componentes.all()
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super(ComponenteCreateView, self).get_context_data(**kwargs)
+        context['accion']="Registrar"
+        context['titulo']="Componente"
+        return context
+
+class ComponenteUpdateView(BSModalUpdateView):
+    model = RelacionMaterialComponente
+    template_name = "includes/formulario generico.html"
+    form_class = RelacionMaterialComponenteForm
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('material_app:material_detalle', kwargs={'pk':self.object.material.id})
+
+    def form_valid(self, form):
+        registro_guardar(form.instance, self.request)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(ComponenteUpdateView, self).get_context_data(**kwargs)
+        context['accion']="Actualizar"
+        context['titulo']="Componentes"
+        return context
+
+class ComponenteDeleteView(BSModalDeleteView):
+    model = RelacionMaterialComponente
+    template_name = "includes/eliminar generico.html"
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('material_app:material_detalle', kwargs={'pk':self.object.material.id})
+
+    def get_context_data(self, **kwargs):
+        context = super(ComponenteDeleteView, self).get_context_data(**kwargs)
+        context['accion']="Eliminar"
+        context['titulo']="Componente"
+        return context
+
+class EspecificacionCreateView(BSModalCreateView):
+    model = Especificacion
+    template_name = "includes/formulario generico.html"
+    form_class = EspecificacionForm
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('material_app:material_detalle', kwargs={'pk':self.kwargs['material_id']})
+
+    def form_valid(self, form):
+        form.instance.material = Material.objects.get(id = self.kwargs['material_id'])
+        form.instance.usuario = self.request.user
+        registro_guardar(form.instance, self.request)
+        return super().form_valid(form)
+
+    def get_form_kwargs(self, *args, **kwargs):
+        material = Material.objects.get(id = self.kwargs['material_id'])
+        kwargs = super(EspecificacionCreateView, self).get_form_kwargs(*args, **kwargs)
+        kwargs['atributos'] = material.subfamilia.familia.atributos.all()
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super(EspecificacionCreateView, self).get_context_data(**kwargs)
+        context['accion']="Registrar"
+        context['titulo']="Especificación"
+        return context     
+
+class EspecificacionUpdateView(BSModalUpdateView):
+    model = Especificacion
+    template_name = "includes/formulario generico.html"
+    form_class = EspecificacionForm
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('material_app:material_detalle', kwargs={'pk':self.object.material.id})
+
+    def form_valid(self, form):
+        registro_guardar(form.instance, self.request)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(EspecificacionUpdateView, self).get_context_data(**kwargs)
+        context['accion']="Actualizar"
+        context['titulo']="Especificación"
+        return context
+
+class EspecificacionDeleteView(BSModalDeleteView):
+    model = Especificacion
+    template_name = "includes/eliminar generico.html"
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('material_app:material_detalle', kwargs={'pk':self.object.material.id})
+
+    def get_context_data(self, **kwargs):
+        context = super(EspecificacionDeleteView, self).get_context_data(**kwargs)
+        context['accion']="Eliminar"
+        context['titulo']="Especificación"
+        return context
+
+class DatasheetCreateView(BSModalCreateView):
+    model = Datasheet
+    template_name = "includes/formulario generico.html"
+    form_class = DatasheetForm
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('material_app:material_detalle', kwargs={'pk':self.kwargs['material_id']})
+
+    def form_valid(self, form):
+        form.instance.material = Material.objects.get(id = self.kwargs['material_id'])
+
+        form.instance.usuario = self.request.user
+        registro_guardar(form.instance, self.request)
+
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(DatasheetCreateView, self).get_context_data(**kwargs)
+        context['accion']="Registrar"
+        context['titulo']="Datasheet"
+        return context     
+
+class DatasheetUpdateView(BSModalUpdateView):
+    model = Datasheet
+    template_name = "includes/formulario generico.html"
+    form_class = DatasheetForm
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('material_app:material_detalle', kwargs={'pk':self.object.material.id})
+
+    def form_valid(self, form):
+        registro_guardar(form.instance, self.request)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(DatasheetUpdateView, self).get_context_data(**kwargs)
+        context['accion']="Actualizar"
+        context['titulo']="Especificación"
+        return context
+
+class DatasheetDeleteView(BSModalDeleteView):
+    model = Datasheet
+    template_name = "includes/eliminar generico.html"
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('material_app:material_detalle', kwargs={'pk':self.object.material.id})
+
+    def get_context_data(self, **kwargs):
+        context = super(DatasheetDeleteView, self).get_context_data(**kwargs)
+        context['accion']="Eliminar"
+        context['titulo']="Especificación"
+        return context
+
+class DatosImportacionUpdateView(BSModalUpdateView):
+    model = Material
+    template_name = "includes/formulario generico.html"
+    form_class = DatosImportacionForm
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('material_app:material_detalle', kwargs={'pk':self.object.id})
+
+    def form_valid(self, form):
+        registro_guardar(form.instance, self.request)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(DatosImportacionUpdateView, self).get_context_data(**kwargs)
+        context['accion']="Actualizar"
+        context['titulo']="Importaciones"
+        return context
+
+class ProductoSunatUpdateView(BSModalUpdateView):
+    model = Material
+    template_name = "material/material/form.html"
+    form_class = ProductoSunatForm
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('material_app:material_detalle', kwargs={'pk':self.object.id})
+
+    def form_valid(self, form):
+        registro_guardar(form.instance, self.request)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductoSunatUpdateView, self).get_context_data(**kwargs)
+        context['accion']="Actualizar"
+        context['titulo']="Producto Sunat"
+        return context
+
+class FamiliaSunatForm(forms.Form):
+    familia = forms.ModelChoiceField(queryset = FamiliaSunat.objects.all(), required=False, empty_label=None)
+
+def FamiliaSunatView(request, id_segmento):
+    form = FamiliaSunatForm()
+    form.fields['familia'].queryset = FamiliaSunat.objects.filter(segmento = id_segmento)
+    return render(request, 'includes/form.html', context={'form':form})
+
+class ClaseSunatForm(forms.Form):
+    clase = forms.ModelChoiceField(queryset = ClaseSunat.objects.all(), required=False, empty_label=None)
+
+def ClaseSunatView(request, id_familia):
+    form = ClaseSunatForm()
+    form.fields['clase'].queryset = ClaseSunat.objects.filter(familia = id_familia)
+    return render(request, 'includes/form.html', context={'form':form})
+
+class ProductoSunatForm(forms.Form):
+    producto = forms.ModelChoiceField(queryset = ProductoSunat.objects.all(), required=False, empty_label=None)
+
+def ProductoSunatView(request, id_clase):
+    form = ProductoSunatForm()
+    form.fields['producto'].queryset = ProductoSunat.objects.filter(clase = id_clase)
+    return render(request, 'includes/form.html', context={'form':form})
