3eee1b5fed093e0403d7618540d6dd2f95cb04b0
Tim Robles <robles.tim@gmail.com>
2022-05-23 18:46:18 -0500

    Cambios para Sede

--- applications/sede/forms.py
+++ applications/sede/forms.py
@@ -7,8 +7,8 @@ from bootstrap_modal_forms.forms import  BSModalModelForm
 
 class SedeCreateForm(BSModalModelForm):
     departamento_buscar = forms.ModelChoiceField(label = 'Departamento', queryset = Departamento.objects.all())
-    provincia_buscar = forms.ModelChoiceField(label = 'Provincia', queryset = Provincia.objects.all(), required=False)
-    distrito_buscar = forms.ModelChoiceField(label = 'Distrito', queryset = Distrito.objects.all(), required=False)
+    provincia_buscar = forms.ModelChoiceField(label = 'Provincia', queryset = Provincia.objects.none())
+    distrito_buscar = forms.ModelChoiceField(label = 'Distrito', queryset = Distrito.objects.none())
     class Meta:
         model = Sede
         fields = (
@@ -25,6 +25,20 @@ class SedeCreateForm(BSModalModelForm):
             'sociedad': forms.CheckboxSelectMultiple(),
         }
 
+    def clean_departamento_buscar(self):
+        departamento_buscar = self.cleaned_data.get('departamento_buscar')
+        provincia_buscar = self.fields['provincia_buscar']
+        provincia_buscar.queryset = Provincia.objects.filter(departamento = departamento_buscar.codigo)
+    
+        return departamento_buscar
+
+    def clean_provincia_buscar(self):
+        provincia_buscar = self.cleaned_data.get('provincia_buscar')
+        distrito_buscar = self.fields['distrito_buscar']
+        distrito_buscar.queryset = Distrito.objects.filter(provincia = provincia_buscar.codigo)
+    
+        return provincia_buscar
+
     def __init__(self, *args, **kwargs):
         super(SedeCreateForm, self).__init__(*args, **kwargs)
         self.fields['usuario_responsable'].queryset = get_user_model().objects.filter(is_active=1)  
@@ -34,9 +48,9 @@ class SedeCreateForm(BSModalModelForm):
         self.fields['sociedad'].widget.attrs['class'] = 'nobull'
 
 class SedeUpdateForm(BSModalModelForm):
-    departamento_buscar = forms.ModelChoiceField(label = 'Departamento', queryset = Departamento.objects.all())
-    provincia_buscar = forms.ModelChoiceField(label = 'Provincia', queryset = Provincia.objects.all(), required=False)
-    distrito_buscar = forms.ModelChoiceField(label = 'Distrito', queryset = Distrito.objects.all(), required=False)
+    departamento_buscar = forms.ModelChoiceField(label = 'Departamento', queryset = Departamento.objects.none())
+    provincia_buscar = forms.ModelChoiceField(label = 'Provincia', queryset = Provincia.objects.none())
+    distrito_buscar = forms.ModelChoiceField(label = 'Distrito', queryset = Distrito.objects.none())
     class Meta:
         model = Sede
         fields = (
@@ -51,9 +65,14 @@ class SedeUpdateForm(BSModalModelForm):
     def __init__(self, *args, **kwargs):
         super(SedeUpdateForm, self).__init__(*args, **kwargs)
         self.fields['usuario_responsable'].queryset = get_user_model().objects.filter(is_active=1) 
-        ubigeo = self.instance.ubigeo
-        self.fields['departamento_buscar'].initial = ubigeo[:2]
-        self.fields['provincia_buscar'].initial = ubigeo[:4]
-        self.fields['distrito_buscar'].initial = ubigeo
+        distrito = self.instance.distrito
+        provincia = distrito.provincia
+        departamento = provincia.departamento
+        self.fields['departamento_buscar'].initial = departamento
+        self.fields['provincia_buscar'].queryset = Provincia.objects.filter(departamento = departamento.codigo)
+        self.fields['provincia_buscar'].initial = provincia
+        self.fields['distrito_buscar'].queryset = Distrito.objects.filter(provincia = provincia.codigo)
+        self.fields['distrito_buscar'].initial = distrito
+        
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
