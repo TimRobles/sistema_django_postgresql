d1d338fe38c7e542757e5243b5dc253e989f86f6
TimRobles <robles.tim@gmail.com>
2022-06-23 01:33:05 -0500

    Texto csv

--- applications/sorteo/models.py
+++ applications/sorteo/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 # SELECT Nro_Ticket, Razon_Social, RUC, Nombre_Contacto FROM `TAB_MARKETING_004_Tickets_Sorteo` WHERE Nro_Sorteo=1 ORDER BY `Nro_Ticket` ASC
+# \copy sorteo_ticket(ticket, razon_social, ruc, contacto, elegido) from /webapps/archivos/TAB_MARKETING_004_Tickets_Sorteo.csv DELIMITER ';' HEADER CSV;
 class Ticket(models.Model):
     ticket = models.CharField('Ticket', max_length=4)
     razon_social = models.CharField('Razón Social', max_length=100)
