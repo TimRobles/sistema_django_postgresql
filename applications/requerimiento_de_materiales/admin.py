59f047c4ea21f8ebfd75d1368c648446f69d3cf4
DianaSilvaD <silvaelizabethdiana@gmail.com>
2022-06-30 15:07:11 -0500

    eliminación de requerimiento_material

--- applications/requerimiento_de_materiales/admin.py
+++ applications/requerimiento_de_materiales/admin.py
@@ -4,8 +4,6 @@ from .models import (
     ListaRequerimientoMaterialDetalle,    
     RequerimientoMaterialProveedor,
     RequerimientoMaterialProveedorDetalle,
-    RequerimientoMaterial,
-    RequerimientoMaterialDetalle,
 )
 
 class ListaRequerimientoMaterialAdmin(admin.ModelAdmin):
@@ -72,44 +70,7 @@ class RequerimientoMaterialProveedorDetalleAdmin(admin.ModelAdmin):
 
 
 
-
-class RequerimientoMaterialAdmin(admin.ModelAdmin):
-    list_display = (
-        'titulo',
-        'fecha',
-        'proveedor',
-        'interlocutor_proveedor',
-        'estado',
-        'created_at',
-        'updated_at',
-        )
-
-    def save_model(self, request, obj, form, change):
-        if obj.created_by == None:
-            obj.created_by = request.user
-        obj.updated_by = request.user
-        super().save_model(request, obj, form, change)
-
-class RequerimientoMaterialDetalleAdmin(admin.ModelAdmin):
-    list_display = (
-        'item',
-        'content_type',
-        'id_registro',
-        'cantidad',
-        'requerimiento_material',
-        )
-
-    def save_model(self, request, obj, form, change):
-        if obj.created_by == None:
-            obj.created_by = request.user
-        obj.updated_by = request.user
-        super().save_model(request, obj, form, change)
-
-
 admin.site.register(ListaRequerimientoMaterial, ListaRequerimientoMaterialAdmin)
 admin.site.register(ListaRequerimientoMaterialDetalle, ListaRequerimientoMaterialDetalleAdmin)
 admin.site.register(RequerimientoMaterialProveedor, RequerimientoMaterialProveedorAdmin)
 admin.site.register(RequerimientoMaterialProveedorDetalle, RequerimientoMaterialProveedorDetalleAdmin)
-
-# admin.site.register(RequerimientoMaterial, RequerimientoMaterialAdmin)
-# admin.site.register(RequerimientoMaterialDetalle, RequerimientoMaterialDetalleAdmin)
--- applications/requerimiento_de_materiales/models.py
+++ applications/requerimiento_de_materiales/models.py
@@ -105,61 +105,6 @@ class RequerimientoMaterialProveedorDetalle(models.Model):
         return str(self.cantidad)
 
 
-
-
-class RequerimientoMaterial(models.Model):
-    ESTADOS_REQUERIMIENTO = (
-        (1, 'BORRADOR'),
-        (2, 'PENDIENTE'),
-        (3, 'ENVIADO'),
-    )
-
-    fecha = models.DateField('Fecha', auto_now=False, auto_now_add=True, blank=True, null=True, editable=False)
-    titulo = models.CharField('Titulo', max_length=150,null=True, blank=True)
-    proveedor = models.ForeignKey(Proveedor, on_delete=models.PROTECT, related_name='Proveedor',null=True, blank=True)
-    interlocutor_proveedor = models.ForeignKey(InterlocutorProveedor, on_delete=models.PROTECT, related_name='InterlocutorProveedor',null=True, blank=True)
-    requerimiento_material_anterior = models.ForeignKey('self', on_delete=models.CASCADE,null=True, blank=True)
-    version = models.IntegerField(default=0)
-    slug = models.SlugField(null=True, blank=True)
-    estado = models.IntegerField('Estado', choices=ESTADOS_REQUERIMIENTO, default=1)
-
-    created_at = models.DateTimeField('Fecha de Creación', auto_now=False, auto_now_add=True, editable=False)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, blank=True, null=True, related_name='RequerimientoMaterial_created_by', editable=False)
-    updated_at = models.DateTimeField('Fecha de Modificación', auto_now=True, auto_now_add=False, blank=True, null=True, editable=False)
-    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, blank=True, null=True, related_name='RequerimientoMaterial_updated_by', editable=False)
-
-    class Meta:
-        verbose_name = 'Requerimiento de Material'
-        verbose_name_plural = 'Requerimiento de Materiales'
-        ordering = [
-            '-fecha',
-        ]
-    def __str__(self):
-        return str(self.titulo)
-
-class RequerimientoMaterialDetalle(models.Model):
-    item = models.IntegerField(null=True, blank=True)
-    content_type = models.ForeignKey(ContentType, on_delete=models.PROTECT)
-    id_registro = models.IntegerField()
-    cantidad = models.DecimalField('Cantidad', max_digits=22, decimal_places=10, null=True, blank=True)
-    requerimiento_material = models.ForeignKey(RequerimientoMaterial, on_delete=models.CASCADE, related_name='RequerimientoMaterialDetalle_requerimiento_material')
-
-    created_at = models.DateTimeField('Fecha de Creación', auto_now=False, auto_now_add=True, editable=False)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, blank=True, null=True, related_name='RequerimientoMaterialDetalle_created_by', editable=False)
-    updated_at = models.DateTimeField('Fecha de Modificación', auto_now=True, auto_now_add=False, blank=True, null=True, editable=False)
-    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, blank=True, null=True, related_name='RequerimientoMaterialDetalle_updated_by', editable=False)
-
-    class Meta:
-        verbose_name = 'Requerimiento de Material Detalle'
-        verbose_name_plural = 'Requerimiento de Materiales Detalle'
-        ordering = [
-            'item',
-        ]
-
-    def __str__(self):
-        return str(self.cantidad)
-
-
 def requerimiento_material_proveedor_post_save(*args, **kwargs):
     if kwargs['created']:
         obj = kwargs['instance']
