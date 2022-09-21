9c5a2aea8e94ddc708ad62da2638a0bbe4b8d0bf
Tim Robles <robles.tim@gmail.com>
2022-09-21 09:56:04 -0500

    Moneda en Confirmación

--- applications/cotizacion/models.py
+++ applications/cotizacion/models.py
@@ -181,6 +181,7 @@ class ConfirmacionVenta(models.Model):
     sociedad = models.ForeignKey(Sociedad, on_delete=models.PROTECT)
     tipo_cambio = models.ForeignKey(TipoCambio, on_delete=models.PROTECT, related_name='ConfirmacionVenta_tipo_cambio')
     observacion = models.TextField(blank=True, null=True)
+    moneda = models.ForeignKey(Moneda, on_delete=models.PROTECT, default=1)
     condiciones_pago = models.CharField('Condiciones de Pago', max_length=50, blank=True, null=True)
     tipo_venta = models.IntegerField('Tipo de Venta', choices=TIPO_VENTA, default=1)
     descuento_global = models.DecimalField('Descuento Global', max_digits=14, decimal_places=2, default=0)
