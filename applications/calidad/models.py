b3bd9f84e0237cfc54d00bdf25d6653d13fdbdbb
RonnyOre <ronny.oa.14@gmail.com>
2022-11-14 18:20:15 -0500

    Nota Control Calidad Stock

--- applications/calidad/models.py
+++ applications/calidad/models.py
@@ -3,6 +3,25 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from applications.sociedad.models import Sociedad
 from applications.material.models import SubFamilia
+from applications.nota_ingreso.models import NotaIngreso, NotaIngresoDetalle
+
+class FallaMaterial(models.Model):
+    sub_familia = models.ForeignKey(SubFamilia, on_delete=models.CASCADE)
+    titulo = models.CharField('Titulo', max_length=50)
+    comentario = models.TextField()
+    visible = models.BooleanField()
+  
+    created_at = models.DateTimeField('Fecha de Creación', auto_now=False, auto_now_add=True, editable=False)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, blank=True, null=True, related_name='FallaMaterial_created_by', editable=False)
+    updated_at = models.DateTimeField('Fecha de Modificación', auto_now=True, auto_now_add=False, blank=True, null=True, editable=False)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, blank=True, null=True, related_name='FallaMaterial_updated_by', editable=False)
+
+    class Meta:
+        verbose_name = 'Falla Material'
+        verbose_name_plural = 'Fallas Materiales'
+
+    def __str__(self):
+        return str(self.id)
 
 class EstadoSerie(models.Model):
     numero_estado = models.IntegerField()
@@ -19,7 +38,6 @@ class EstadoSerie(models.Model):
 
     def __str__(self):
         return str(self.id)
-
 
 class Serie(models.Model):
     serie_base = models.CharField('Serie Base', max_length=200)
@@ -41,27 +59,6 @@ class Serie(models.Model):
     def __str__(self):
         return str(self.id)
 
-
-class FallaMaterial(models.Model):
-    sub_familia = models.ForeignKey(SubFamilia, on_delete=models.CASCADE)
-    titulo = models.CharField('Titulo', max_length=50)
-    comentario = models.TextField()
-    visible = models.BooleanField()
-  
-    created_at = models.DateTimeField('Fecha de Creación', auto_now=False, auto_now_add=True, editable=False)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, blank=True, null=True, related_name='FallaMaterial_created_by', editable=False)
-    updated_at = models.DateTimeField('Fecha de Modificación', auto_now=True, auto_now_add=False, blank=True, null=True, editable=False)
-    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, blank=True, null=True, related_name='FallaMaterial_updated_by', editable=False)
-
-    class Meta:
-        verbose_name = 'Falla Material'
-        verbose_name_plural = 'Fallas Materiales'
-
-    def __str__(self):
-        return str(self.id)
-
-
-
 class HistorialEstadoSerie(models.Model):
     serie = models.ForeignKey(Serie, on_delete=models.CASCADE)
     estado_serie = models.ForeignKey(EstadoSerie, on_delete=models.CASCADE)
@@ -76,6 +73,53 @@ class HistorialEstadoSerie(models.Model):
     class Meta:
         verbose_name = 'Historial Estado Serie'
         verbose_name_plural = 'Historial Estado Series'
+
+    def __str__(self):
+        return str(self.id)
+
+class NotaControlCalidadStock(models.Model):
+    ESTADOS_NOTA_CALIDAD_STOCK = [
+    (1, 'EN PROCESO'),
+    (2, 'CONCLUIDA'),
+    (3, 'ANULADA'),
+    ]
+    nro_nota_calidad = models.CharField('Nro. Nota Calidad', max_length=50, blank=True, null=True)
+    nota_ingreso = models.ForeignKey(NotaIngreso, on_delete=models.CASCADE)
+    motivo_anulacion = models.TextField('Motivo de Anulación', blank=True, null=True)
+    comentario = models.TextField(blank=True, null=True)
+    estado = models.IntegerField(choices=ESTADOS_NOTA_CALIDAD_STOCK, default=1)
+
+    created_at = models.DateTimeField('Fecha de Creación', auto_now=False, auto_now_add=True, editable=False)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, blank=True, null=True, related_name='NotaControlCalidadStock_created_by', editable=False)
+    updated_at = models.DateTimeField('Fecha de Modificación', auto_now=True, auto_now_add=False, blank=True, null=True, editable=False)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, blank=True, null=True, related_name='NotaControlCalidadStock_updated_by', editable=False)
+
+    class Meta:
+        verbose_name = 'Nota Control Calidad Stock'
+        verbose_name_plural = 'Notas Control Calidad Stock'
+
+    def __str__(self):
+        return str(self.id)
+
+class NotaControlCalidadStockDetalle(models.Model):
+    ESTADOS_INSPECCION = [
+    (1, 'BUENO'),
+    (2, 'DAÑADO'),
+    ]
+    item = models.IntegerField(blank=True, null=True)
+    nota_ingreso_detalle = models.ForeignKey(NotaIngresoDetalle, on_delete=models.CASCADE)
+    cantidad_calidad = models.DecimalField('Cantidad', max_digits=22, decimal_places=10, default=0)
+    inspeccion = models.IntegerField('Estado Inspección',choices=ESTADOS_INSPECCION, default=1)
+    nota_control_calidad_stock = models.ForeignKey(NotaControlCalidadStock, on_delete=models.CASCADE, related_name='NotaControlCalidadStockDetalle_nota_control_calidad_stock')
+
+    created_at = models.DateTimeField('Fecha de Creación', auto_now=False, auto_now_add=True, editable=False)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, blank=True, null=True, related_name='NotaControlCalidadStockDetalle_created_by', editable=False)
+    updated_at = models.DateTimeField('Fecha de Modificación', auto_now=True, auto_now_add=False, blank=True, null=True, editable=False)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, blank=True, null=True, related_name='NotaControlCalidadStockDetalle_updated_by', editable=False)
+
+    class Meta:
+        verbose_name = 'Nota Control Calidad Stock Detalle'
+        verbose_name_plural = 'Notas Control Calidad Stock Detalle'
 
     def __str__(self):
         return str(self.id)
