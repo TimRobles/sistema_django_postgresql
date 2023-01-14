73382b6ddc0ca15621d37e9afd8f30277a7eb1eb
Tim Robles <robles.tim@gmail.com>
2023-01-14 13:21:23 -0500

    Confirmación de reserva por venta

--- applications/logistica/views.py
+++ applications/logistica/views.py
@@ -778,11 +778,18 @@ class NotaSalidaConcluirView(PermissionRequiredMixin, BSModalDeleteView):
                         documento_anterior = self.object.solicitud_prestamo_materiales
                         estado_serie = EstadoSerie.objects.get(numero_estado=9)
                     else:
-                        print('Confirmación')
-                        movimiento_inicial = TipoMovimiento.objects.get(codigo=120)  # Confirmado por venta
-                        movimiento_final = TipoMovimiento.objects.get(codigo=121)  # Salida por venta
-                        documento_anterior = self.object.confirmacion_venta
-                        estado_serie = EstadoSerie.objects.get(numero_estado=3)
+                        if self.object.confirmacion_venta.cotizacion_venta.estado == 4: #Confirmado con Reserva
+                            print('Confirmación')
+                            movimiento_inicial = TipoMovimiento.objects.get(codigo=119)  # Confirmación de reserva por venta
+                            movimiento_final = TipoMovimiento.objects.get(codigo=121)  # Salida por venta
+                            documento_anterior = self.object.confirmacion_venta
+                            estado_serie = EstadoSerie.objects.get(numero_estado=3)
+                        elif self.object.confirmacion_venta.cotizacion_venta.estado == 5: #Confirmado sin Reserva
+                            print('Confirmación')
+                            movimiento_inicial = TipoMovimiento.objects.get(codigo=120)  # Confirmado por venta
+                            movimiento_final = TipoMovimiento.objects.get(codigo=121)  # Salida por venta
+                            documento_anterior = self.object.confirmacion_venta
+                            estado_serie = EstadoSerie.objects.get(numero_estado=3)
 
                     consolidado = {}
                     for detalle in detalles:
