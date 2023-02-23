7076be5cc0588fcf7dc41413911374c8618cf627
TimRobles <robles.tim@gmail.com>
2023-02-23 13:18:05 -0500

    /webapps/sistema_django_prod

--- sistema_django_postgresql/settings/base.py
+++ sistema_django_postgresql/settings/base.py
@@ -97,8 +97,8 @@ MIDDLEWARE = [
 ]
 
 CRONJOBS = [
-    ('45 13 * * *', 'applications.reportes.pdf.reporte_cobranza', '>> /home/tim/Documents/github/sistema_django_postgresql/cronjob.log 2>&1'),
-    ('* * * * *', 'applications.reportes.cron.funcion_iterativa', '>> /home/tim/Documents/github/sistema_django_postgresql/cronjob_itera.log 2>&1'),
+    ('45 13 * * *', 'applications.reportes.pdf.reporte_cobranza', '>> /webapps/sistema_django_prod/sistema_django_postgresql/cronjob.log 2>&1'),
+    ('* * * * *', 'applications.reportes.cron.funcion_iterativa', '>> /webapps/sistema_django_prod/sistema_django_postgresql/cronjob_itera.log 2>&1'),
 ]
 
 ROOT_URLCONF = 'sistema_django_postgresql.urls'
