3dcfa08621c6340c063f0a515dca7ad83aa2c7c5
TimRobles <robles.tim@gmail.com>
2023-03-28 15:07:45 -0500

    Garantía

--- applications/garantia/admin.py
+++ applications/garantia/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import(
+    CondicionesGarantia,
     IngresoReclamoGarantia,
     IngresoReclamoGarantiaDetalle,
     ControlCalidadReclamoGarantia,
@@ -148,6 +149,24 @@ class SalidaReclamoGarantiaAdmin(admin.ModelAdmin):
         'fecha_salida',
         'observacion',
         'estado',
+        'created_at',
+        'created_by',
+        'updated_at',
+        'updated_by',
+    )
+
+    def save_model(self, request, obj, form, change):
+        if obj.created_by == None:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(CondicionesGarantia)
+class CondicionesGarantiaAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'condicion',
         'created_at',
         'created_by',
         'updated_at',
