f1f782bbf90feb71ae918f4c926965a092b4e706
TimRobles <robles.tim@gmail.com>
2023-07-05 12:13:59 -0500

    funciones

--- applications/contabilidad/funciones.py
+++ applications/contabilidad/funciones.py
@@ -1,7 +1,8 @@
 from decimal import Decimal
 from django.db import models
+from applications.caja_chica.models import ReciboCajaChica, Requerimiento
 from applications.importaciones import *
-from applications.contabilidad.models import EsSalud
+from applications.contabilidad.models import ChequeFisico, ChequeVueltoExtra, EsSalud, ReciboBoletaPago, ReciboServicio
 from applications.datos_globales.models import RemuneracionMinimaVital
 
 
@@ -60,3 +61,24 @@ def calcular_datos_boleta(obj):
     if not obj.impuesto_quinta:
         obj.impuesto_quinta = Decimal('0.00')
     obj.neto_recibido = obj.haber_mensual + obj.compra_vacaciones + obj.lic_con_goce_haber + obj.dominical + obj.movilidad + obj.asig_familiar + obj.vacaciones + obj.gratificacion + obj.ley29351 + obj.cts + obj.bonif_1mayo - (obj.aporte_obligatorio + obj.comision + obj.prima_seguro + obj.impuesto_quinta)
+
+
+def movimientos_cheque(cheque):
+    recibos_boleta_pago = ReciboBoletaPago.objects.filter(content_type = ContentType.objects.get_for_model(cheque), id_registro = cheque.id)
+    recibos_servicio = ReciboServicio.objects.filter(content_type = ContentType.objects.get_for_model(cheque), id_registro = cheque.id)
+    recibos_caja_chica = ReciboCajaChica.objects.filter(cheque = cheque)
+    requerimientos = Requerimiento.objects.filter(content_type = ContentType.objects.get_for_model(cheque), id_registro = cheque.id)
+    cheques_fisicos = ChequeFisico.objects.filter(cheque = cheque)
+    vuelto_extra = ChequeVueltoExtra.objects.filter(cheque = cheque)
+    total_boleta_pago = recibos_boleta_pago.aggregate(models.Sum('monto'))['monto__sum']
+    total_boleta_pago_pagado = recibos_boleta_pago.aggregate(models.Sum('monto_pagado'))['monto_pagado__sum']
+    total_servicio = recibos_servicio.aggregate(models.Sum('monto'))['monto__sum']
+    total_servicio_pagado = recibos_servicio.aggregate(models.Sum('monto_pagado'))['monto_pagado__sum']
+    total_caja_chica = recibos_caja_chica.aggregate(models.Sum('monto'))['monto__sum']
+    total_caja_chica_pagado = recibos_caja_chica.aggregate(models.Sum('monto_pagado'))['monto_pagado__sum']
+    total_requerimiento = requerimientos.aggregate(models.Sum('monto'))['monto__sum']
+    total_requerimiento_usado = requerimientos.aggregate(models.Sum('monto_usado'))['monto_usado__sum']
+    total_cheque_fisico = cheques_fisicos.aggregate(models.Sum('monto'))['monto__sum']
+    total_cheque_fisico_comision = cheques_fisicos.aggregate(models.Sum('comision'))['comision__sum']
+    total_cheque_fisico_recibido = cheques_fisicos.aggregate(models.Sum('monto_recibido'))['monto_recibido__sum']
+    total_monto_requerido = total_boleta_pago + total_servicio + total_caja_chica + total_requerimiento
