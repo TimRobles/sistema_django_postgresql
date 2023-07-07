8bd92ae7e67aee850f8ec88ae3adb41c1638b1fc
TimRobles <robles.tim@gmail.com>
2023-07-07 17:37:24 -0500

    Imagenes

--- applications/cotizacion/models.py
+++ applications/cotizacion/models.py
@@ -145,15 +145,17 @@ class CotizacionVenta(models.Model):
 
 def cotizacion_venta_post_save(*args, **kwargs):
     obj = kwargs['instance']
-    applications.crm.models.actualizar_estado_cliente_crm(obj.cliente.id)
+    if obj.estado > 1:
+        applications.crm.models.actualizar_estado_cliente_crm(obj.cliente.id)
 
 def cotizacion_venta_pre_save(*args, **kwargs):
     obj = kwargs['instance']
-    obj2 = CotizacionVenta.objects.get(id=obj.id)
-    applications.crm.models.actualizar_estado_cliente_crm(obj2.cliente.id)
+    if obj.estado > 1:
+        obj2 = CotizacionVenta.objects.get(id=obj.id)
+        applications.crm.models.actualizar_estado_cliente_crm(obj2.cliente.id)
 
-# post_save.connect(cotizacion_venta_post_save, sender=CotizacionVenta)
-# pre_save.connect(cotizacion_venta_pre_save, sender=CotizacionVenta)
+post_save.connect(cotizacion_venta_post_save, sender=CotizacionVenta)
+pre_save.connect(cotizacion_venta_pre_save, sender=CotizacionVenta)
         
 
 class CotizacionVentaDetalle(models.Model):
