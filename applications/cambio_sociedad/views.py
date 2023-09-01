76df50801463474eb6439752c122fffbf7da3f48
TimRobles <robles.tim@gmail.com>
2023-09-01 10:08:36 -0500

    lala

--- applications/cambio_sociedad/views.py
+++ applications/cambio_sociedad/views.py
@@ -364,10 +364,6 @@ class ValidarSeriesCambioSociedadStockDetailView(PermissionRequiredMixin, FormVi
                     form.add_error('serie', "Serie ya ha sido registrada")
                     return super().form_invalid(form)
 
-                if buscar.estado != 'DISPONIBLE' and buscar.estado != 'REPARADO':
-                    if cambio_sociedad_stock_detalle.tipo_stock_inicial.id != 6:
-                        form.add_error('serie', "Serie no disponible, su estado es: %s" % buscar.estado)
-                        return super().form_invalid(form)
             except:
                 form.add_error('serie', "Serie no encontrada: %s" % serie)
                 return super().form_invalid(form)
