4486e23dbc1ce578feea490d563ed83ef2bbb962
Tim Robles <151044859+TimRoblesM@users.noreply.github.com>
2025-03-28 12:16:45 -0500

    sin excel2img

--- applications/reportes/excel_img.py
+++ applications/reportes/excel_img.py
@@ -1,10 +1,2 @@
-from django.conf import settings
-
-if settings.DEBUG:
-    import excel2img
-
-    def convertir_excel_a_imagen(excel_path, img_path, label, rango):
-        return excel2img.export_img(excel_path, img_path, label, rango)
-else:
-    def convertir_excel_a_imagen(excel_path, img_path, label, rango):
-        return None
+def convertir_excel_a_imagen(excel_path, img_path, label, rango):
+    return None
