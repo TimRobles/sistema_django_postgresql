03553e6cf25ece25708d5db7710946c7b22f3232
Tim Robles <151044859+TimRoblesM@users.noreply.github.com>
2025-04-02 14:44:33 -0500

    Update excel_img.py

--- applications/reportes/excel_img.py
+++ applications/reportes/excel_img.py
@@ -1,10 +1,4 @@
-from django.conf import settings
+import excel2img
 
-if settings.DEBUG:
-    import excel2img
-
-    def convertir_excel_a_imagen(excel_path, img_path, label, rango):
-        return excel2img.export_img(excel_path, img_path, label, rango)
-else:
-    def convertir_excel_a_imagen(excel_path, img_path, label, rango):
-        return None
+def convertir_excel_a_imagen(excel_path, img_path, label, rango):
+    return excel2img.export_img(excel_path, img_path, label, rango)
