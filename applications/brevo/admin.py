cb66a04d53814ed4997cbdf13e49267e03efa953
Tim Robles <151044859+TimRoblesM@users.noreply.github.com>
2025-04-07 23:06:16 -0500

    admin brevo

--- applications/brevo/admin.py
+++ applications/brevo/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class BrevoWebhookEventAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'event',
+        'email',
+        'sender_email',
+        'subject',
+        'date',
+        'ts',
+        'ts_event',
+        'ts_epoch',
+        'message_id',
+        'sending_ip',
+        'reason',
+        'mirror_link',
+        'tag',
+        'created_at'
+    )
+    list_filter = ('event',)
+    search_fields = ('email',)
