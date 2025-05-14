220ce951b980e292381132730766554048c49833
Tim Robles <151044859+TimRoblesM@users.noreply.github.com>
2025-05-14 14:40:48 -0500

    excel img

--- applications/reportes/excel_img.py
+++ applications/reportes/excel_img.py
@@ -1,9 +1,9 @@
-# def convertir_excel_a_imagen(excel_path, img_path, label, rango):
-#     return None
-
-import excel2img
-
 def convertir_excel_a_imagen(excel_path, img_path, label, rango):
-    return excel2img.export_img(excel_path, img_path, label, rango)
+    return None
+
+# import excel2img
+
+# def convertir_excel_a_imagen(excel_path, img_path, label, rango):
+#     return excel2img.export_img(excel_path, img_path, label, rango)
 
 
